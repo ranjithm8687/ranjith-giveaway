8d33c38d5fa32e490552e16afda1eb24ababe1dc
ranjithm8687 <82140911+ranjithm8687@users.noreply.github.com>
2023-06-29 14:47:20 +0530

    feet: javascript dom

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-alert("Hello Ranjith");
+const maiEl = document.querySelector("main");
+const successfulUIEL = document.getElementById("formSubmitted");
+const formEl = document.querySelector("form");
+
+const submitForm = (event) => {
+    event.preventDefault();
+    // console.log("Submited", event);
+
+    const formData = new FormData(formEl);
+
+    console.log([...formData.entries()]);
+    console.log([...formData.values()]);
+    // updated UI Logic
+    // updateUI();
+};
+
+formEl.addEventListener("submit", submitForm);
+
+//UP Date UI
+
+const updateUI = () => {
+    maiEl.classList.add("hidden");
+    successfulUIEL.classList.remove("hidden");
+};
