0066d7f481b550f468468a8ef4b5a1ced208a9fd
ranjithm8687 <82140911+ranjithm8687@users.noreply.github.com>
2023-06-30 17:10:28 +0530

    feet: dynamic conditions

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,22 @@ const maiEl = document.querySelector("main");
 const successfulUIEL = document.getElementById("formSubmitted");
 const formEl = document.querySelector("form");
 
+import conditionList from "./conditions.data.js";
+
+const conditionContainerEl = document.getElementById("conditions");
+//console.log(conditionList);
+(function injectConditions() {
+    const fragment = document.createDocumentFragment();
+
+    // Looping all condition List
+    conditionList.forEach((condition) => {
+        const liEl = document.createElement("li");
+        liEl.textContent = condition;
+        fragment.appendChild(liEl);
+    });
+    conditionContainerEl.appendChild(fragment);
+})();
+
 const validateForm = () => {
     const formInputsEl = formEl.querySelectorAll("input, select, textarea");
 
